7a18fde777ba0b3d7152101d82fabadfdcba23e1
Yohan <yhezkiya@ucsd.edu>
2019-02-13 19:40:30 -0800

    okay

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ range_min = 0.3
 range_max = 50.0
 
 #used to print debug statements
-debug = 0
+debug = False
 
 #going to be used to store the previous arrays that were passed in
 medianArray = [ [0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.],
@@ -14,15 +14,16 @@ medianArray = [ [0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.],
 # Will create arrays and call rangeFilter to test rangeFilter
 # Will also test medianFilter to test temporal median filter
 def test():
-    name = raw_input("Type 'x' for debug mode")
-    if(name == x):
-        debug = 1
+    name = raw_input("Type 'x' for debug mode: ")
+    if(name is 'x'):
+        global debug #modify global debug to be true
+        debug = True
     testArray1 = medianArray[0]
     testArray2 = medianArray[1]
     testArray3 = medianArray[2]
     testArray4 = medianArray[3]
     testArray5 = medianArray[4]
-    rangeFilter(testArray)
+    rangeFilter(testArray1)
 
 # Crops all values below range_min (resp above range_max)
 # and replaces them with the range_min value (resp above range max)
@@ -30,7 +31,7 @@ def test():
 def rangeFilter(arr):
     #simple for loop through the array in log(n) time
     #modifies array[index] to range_min or range_max if out of range
-    if(debug == 1):
+    if(debug):
         print "[Array before rangeFilter: " + arr + "]"
     for index in range(len(arr)):
         if(arr[index] < range_min ):
@@ -46,6 +47,7 @@ def rangeFilter(arr):
 # @param arr the single array passed in to do check on
 # @param D the number of previous scans to look at using medianArray
 def medianFilter(arr, D):
+    print "nothing yet"
     #for loop
 
 
