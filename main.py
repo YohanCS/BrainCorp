d1d55512147025f1f838453b86e3e9199b4202f8
yohan <yhezkiya@ucsd.edu>
2019-02-14 23:53:10 -0800

    Finished testing to ensure functions are working

--- main.py
+++ main.py
@@ -18,32 +18,88 @@ def test():
 
     # tests arrays
     testArray1 = testArrays[0][:] #copy of array instead of pointing to it
-    testArray2 = testArrays[1]
-    testArray3 = testArrays[2]
-    testArray4 = testArrays[3]
-    testArray5 = testArrays[4]
+    testArray2 = [0.02, 0.03, 0.25, 0., 10.]
+    testArray3 = [0.2, 0.3, 40., 100., 29.]
+    testArray4 = [49., 50., 51., 52., 53.]
 
+    # RangeFilter tests
+    print "Array 1 before range filter: ",
+    print testArray1
+    print "Array 1 after range filter: ",
+    print range.update(testArray1)
+    print ""
+
+
+    print "Array 2 before range filter: ",
+    print testArray2
+    print "Array 2 after range filter: ",
+    print range.update(testArray2)
+    print ""
+
+
+    print "Array 3 before range filter: ",
+    print testArray3
+    print "Array 3 after range filter: ",
+    print range.update(testArray3)
+    print ""
+
+
+    print "Array 4 before range filter: ",
+    print testArray4
+    print "Array 4 after range filter: ",
+    print range.update(testArray4)
+    print ""
 
     # calling methods of objects and testing
     #testArray1 = range.update(testArray1)
     #print testArray1
     #testArray1 = testArrays[0][:]
 
-    testArray1 = median.update(testArray1, 3)
-    testArray2 = median.update(testArray2, 3)
-    testArray3 = median.update(testArray3, 3)
-    testArray4 = median.update(testArray4, 3)
-    testArray5 = median.update(testArray5, 3)
-    print "Array 1 : ",
-    print(testArray1)
-    print "Array 2 : ",
-    print(testArray2)
-    print "Array 3 : ",
-    print(testArray3)
-    print "Array 4 : ",
-    print(testArray4)
-    print "Array 5 : ",
-    print(testArray5)
+    # Temporal Median Filter tests
+    # tests arrays
+    testArray1 = testArrays[0][:] #copy of array instead of pointing to it
+    testArray2 = testArrays[1][:]
+    testArray3 = testArrays[2][:]
+    testArray4 = testArrays[3][:]
+    testArray5 = testArrays[4][:]
+
+    time = 0
+    D = 3
+    print "D = " + str(D)  + ", Time = " + str(time) +  "\nArray 1 before median filter: ",
+    print testArray1
+    print "Array 1 after median filter: ",
+    print median.update(testArray1, 3)
+    print ""
+    time = time + 1
+
+    print "D = " + str(D)  + ", Time = " + str(time) +  "\nArray 2 before median filter: ",
+    print testArray2
+    print "Array 2 after median filter: ",
+    print median.update(testArray2, 3)
+    print ""
+    time = time + 1
+
+    print "D = " + str(D)  + ", Time = " + str(time) +  "\nArray 3 before median filter: ",
+    print testArray3
+    print "Array 3 after median filter: ",
+    print median.update(testArray3, 3)
+    print ""
+    time = time + 1
+
+    print "D = " + str(D)  + ", Time = " + str(time) +  "\nArray 4 before median filter: ",
+    print testArray4
+    print "Array 4 after median filter: ",
+    print median.update(testArray4, 3)
+    print ""
+    time = time + 1
+
+    print "D = " + str(D)  + ", Time = " + str(time) +  "\nArray 5 before median filter: ",
+    print testArray5
+    print "Array 5 after median filter: ",
+    print median.update(testArray5, 3)
+    print ""
+    time = time + 1
+
 
 
 
